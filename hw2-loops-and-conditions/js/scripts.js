869cd6d0250515daac6d77291ac406a0bdbb7141
yuriy82791 <yuriy82791@gmail.com>
2020-02-08 11:32:34 +0200

    add check if second number is bigger that first and made some changes in ternary operator

--- hw2-loops-and-conditions/js/scripts.js
+++ hw2-loops-and-conditions/js/scripts.js
@@ -1,5 +1,6 @@
 let sum = null;
 let message = null;
+let temp = null;
 let n = parseInt(prompt('Enter first number', 10));
 while(isNaN(n)){
   n = parseInt(prompt('Enter first number', 10));
@@ -8,14 +9,19 @@ let m = parseInt(prompt('Enter second number', 100));
 while(isNaN(m)){
   m = parseInt(prompt('Enter first number', 100));
 }
-let skipEven = confirm('Skip even numbers?');
+const skipEven = confirm('Skip even numbers?');
+if(n > m){
+  temp = n;
+  n = m;
+  m = temp;
+}
 for(let i = n; i <= m; i++){
   if(skipEven && (i % 2) == 0){
     continue;
   }
   sum += i;
 }
-skipEven ?  message = `Loop from ${n} to ${m} without even numbers is - ${sum}` : message = `Loop from ${n} to ${m} with even numbers is - ${sum}`;
+message = skipEven ? `Loop from ${n} to ${m} without even numbers is - ${sum}` : `Loop from ${n} to ${m} with even numbers is - ${sum}`;
 
 console.log(message);
 document.getElementById('result').innerHTML += message;
