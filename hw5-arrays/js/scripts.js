18b76bf04284de20b9b994fbe0c499efe2c7af3e
yuriy82791 <yuriy82791@gmail.com>
2020-02-12 23:43:53 +0200

    add first function createRandomArray

--- hw5-arrays/js/scripts.js
+++ hw5-arrays/js/scripts.js
@@ -1,3 +1,12 @@
 const getRandomNumber = function(from, to){
   return Math.floor(Math.random() * (to - from + 1)) + from;
 };
+
+const createRandomArray = function(length, from, to){
+  let arr = [];
+  for(let i = 0; i < length; i++){
+    arr.push(getRandomNumber(from, to));
+  }
+  return arr;
+}
+console.log(createRandomArray(20, 10, 1000)); 
