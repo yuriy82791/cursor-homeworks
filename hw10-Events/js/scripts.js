097d5a8c6cf0fd2186354dd0a77380bca118eb8a
yuriy82791 <yuriy82791@gmail.com>
2020-03-09 14:58:00 +0200

    add play sound on click and crate audio using js

--- hw10-Events/js/scripts.js
+++ hw10-Events/js/scripts.js
@@ -1,29 +1,57 @@
-let soundID = null;
-document.addEventListener('keypress', function(event){
+const soundObj = {
+  97: new Audio('./audio/avengers-assemble.mp3'),
+  100: new Audio('./audio/deadpool.mp3'),
+  98: new Audio('./audio/batman.mp3'),
+  103: new Audio('./audio/groot.mp3'),
+  105: new Audio('./audio/ironman.mp3'),
+  118: new Audio('./audio/venom.mp3'),
+  116: new Audio('./audio/thor.mp3'),
+  114: new Audio('./audio/rocket.mp3'),
+  soundDefault: new Audio('./audio/wonder-woman.mp3'),
+}
+const stopSound = (id = null) => {
+  if(id !== null){
+    soundObj[id].pause();
+    soundObj[id].currentTime = 0;
+  }else{
+    soundObj['soundDefault'].pause();
+    soundObj['soundDefault'].currentTime = 0;
+  }
+}
+startPlay = (keyCode) => {
   const activeBtn = document.querySelector('.key.active');
-  const audio = document.getElementById('sound'+event.keyCode);
-
-  if(audio !== null){
+  const audio = soundObj[keyCode];
+  if(audio !== undefined){
     if(soundID != null){
-      document.getElementById('sound'+soundID).pause();
-      document.getElementById('sound'+soundID).currentTime = 0;
-      document.getElementById('soundDefault').pause();
-      document.getElementById('soundDefault').currentTime = 0;
+      stopSound(soundID);
+      soundObj[soundID].pause();
+      soundObj[soundID].currentTime = 0;
     }
-    soundID = event.keyCode;
+    soundID = keyCode;
     if(activeBtn !== null){
       activeBtn.classList.remove('active');
     }
     document.getElementsByClassName('key'+soundID)[0].classList.add('active');
+    stopSound();
     audio.play();
   }else{
     if(activeBtn !== null){
       activeBtn.classList.remove('active');
     }
     if(soundID != null){
-      document.getElementById('sound'+soundID).pause();
-      document.getElementById('sound'+soundID).currentTime = 0;
+      stopSound(soundID);
     }
-    document.getElementById('soundDefault').play();
+    soundObj['soundDefault'].play();
   }
+}
+let soundID = null;
+document.addEventListener('keypress', function(){
+  startPlay(event.keyCode);
 });  
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => {
+  key.addEventListener('click', function(){
+    let id = this.closest('.key').id;
+    startPlay(id);
+  });
+});
